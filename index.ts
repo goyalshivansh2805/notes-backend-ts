8bd2029985804965936c7ba0783a1f7d74135248
goyalshivansh2805 <144095961+goyalshivansh2805@users.noreply.github.com>
2024-10-21 22:52:53 +0530

    Added credentials sharing

--- index.ts
+++ index.ts
@@ -15,7 +15,16 @@ const PORT: number = Number(process.env.PORT) || 5000;
 
 connectToDB();
 
-app.use(cors());
+app.use(cors({
+    origin: function (origin, callback) {
+        if (origin) {
+            callback(null, origin); 
+        } else {
+            callback(new Error('CORS error: Origin not allowed'));
+        }
+    },
+    credentials: true, 
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
