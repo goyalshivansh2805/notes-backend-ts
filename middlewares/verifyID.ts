9f9847f4d6362619ca147e2c4a6439581b90664c
goyalshivansh2805 <144095961+goyalshivansh2805@users.noreply.github.com>
2024-10-22 10:53:02 +0530

    removed httponly in cookie

--- middlewares/verifyID.ts
+++ middlewares/verifyID.ts
@@ -4,26 +4,35 @@ import User from '../models/User';
 import CustomRequest from '../types/customRequest';
 
 const verifyId = async (req: CustomRequest, res: Response, next: NextFunction) => {
-    console.log("verifyId middleware : ", req.cookies);
-    const sessionId = req.cookies?.sessionId;
+    console.log("verifyId middleware : Authorization Header", req.headers.authorization);
+    
+    const authHeader = req.headers.authorization;
+    
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        res.status(401).json({ success: false, message: "Unauthorized: No Authorization header found" });
+        return;
+    }
+    
+    const sessionId = authHeader.split(' ')[1];
     if (!sessionId) {
-        res.status(401).json({success:false, message: "Unauthorized: No Session ID found" });
+        res.status(401).json({ success: false, message: "Unauthorized: Invalid Authorization header" });
         return;
     }
 
     const userId = getSession(sessionId);
-    if (!userId){
-        res.status(401).json({success:false, message: "Unauthorized: Invalid Session ID" });
+    if (!userId) {
+        res.status(401).json({ success: false, message: "Unauthorized: Invalid Session ID" });
         return;
     }
 
     const user = await User.findById(userId);
     if (!user) {
-        res.status(401).json({success:false, message: "Unauthorized: User not found" });
+        res.status(401).json({ success: false, message: "Unauthorized: User not found" });
         return;
     }
 
     req.user = { _id: userId, role: user.role };
+    
     next();
 }
 
